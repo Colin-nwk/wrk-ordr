e1da7ad6eaab84354df0fd20cc81fb64bc86c297
Colin-nwk <collinsnwoko05@gmail.com>
2024-06-28 09:43:56 +0100

    started fileupload

--- src/components/inputs/FileUploadInput.tsx
+++ src/components/inputs/FileUploadInput.tsx
@@ -1,25 +1,86 @@
-// import React from 'react'
-import { Description, Field, Input } from '@headlessui/react'
 
-import { Wrapper } from "../index"
+import { FC, useState, useCallback } from 'react';
+import { useDropzone, Accept } from 'react-dropzone';
+import { useFormContext, Controller } from 'react-hook-form';
 
-const FileUploadInput = () => {
-    return (
-        <div className='w-full bg-white p-4'>
-
-            <Field className="w-full h-full">
-                <Description className="text-sm/6 font-medium  capitalize mb-4">Upload</Description>
-
-                <Wrapper className='border rounded-2xl'>
-
-                    <label htmlFor='upload' className=" w-full h-fit bg-secondary/5 p-5 rounded-xl flex">
-                        <Input name="upload" id="upload" className="hidden" type="file" />
-                    </label>
-                </Wrapper>
-            </Field>
-
-        </div>
-    )
+interface FileUploadInputProps {
+    name: string;
+    label: string;
+    acceptTypes: Accept;
+    maxFiles: number;
+    maxSizeInMB: number;
 }
 
-export default FileUploadInput
+const FileUploadInput: FC<FileUploadInputProps> = ({ name, acceptTypes, maxFiles, maxSizeInMB, label }) => {
+    const { control } = useFormContext();
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        setFiles(acceptedFiles.map(file => Object.assign(file, {
+            preview: URL.createObjectURL(file)
+        })));
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive, fileRejections } = useDropzone({
+        onDrop,
+        accept: acceptTypes,
+        maxFiles: maxFiles,
+        maxSize: maxSizeInMB * 1048576
+    });
+
+    const [files, setFiles] = useState<File[]>([]);
+
+    const removeFile = (fileName: string) => {
+        const newFiles = files.filter(file => file.name !== fileName);
+        setFiles(newFiles);
+    };
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            defaultValue={[]}
+            render={({ field: { onChange, onBlur, value } }) => (
+
+                <div >
+                    <span>{label}</span>
+
+                    <div className='w-full p-4 bg-white rounded-xl'>
+                        <div {...getRootProps()} className="flex w-full p-5 rounded-lg cursor-pointer h-fit bg-secondary/5">
+                            <input {...getInputProps()} onBlur={onBlur} onChange={(event) => {
+                                onChange(event.target.files);
+                                onDrop(Array.from(event.target.files || []));
+                            }} />
+                            {
+                                isDragActive ?
+                                    <p>Drop the files here ...</p> :
+                                    <p>Drag 'n' drop some files here, or click to select files</p>
+                            }
+                        </div>
+                        <ul>
+                            {files.map(file => (
+                                <li key={file.name} className="flex items-center justify-between p-2">
+                                    {file.name} - {Math.round(file.size / 1024 / 1024 * 100) / 100} MB
+                                    <button type="button" onClick={() => removeFile(file.name)}>Remove</button>
+                                </li>
+                            ))}
+                        </ul>
+                        {fileRejections.length > 0 && (
+                            <div>
+                                <p className="text-red-500">Error: Some files were rejected:</p>
+                                <ul>
+                                    {fileRejections.map((rejection, index) => (
+                                        <li key={index}>
+                                            {rejection.file.name}: {rejection.errors.map((error: any) => <span key={error.code}>{error.message}</span>)}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+        />
+    );
+};
+
+export default FileUploadInput;
--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,10 +1,39 @@
+
 import { FileUploadInput } from "../components"
+import { useForm, FormProvider } from 'react-hook-form';
 
 
 const Messages = () => {
+
+
+    const acceptTypes = {
+        'image/png': ['.png', '.webp']
+        // 'application/pdf': ['.pdf'],
+        // 'text/csv': ['.csv'],
+        // 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
+        // 'application/msword': ['.doc'],
+        // 'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx']
+    };
+
+    const methods = useForm({
+        defaultValues: {
+            documents: []
+        }
+    });
     return (
         <div>
-            <FileUploadInput />
+            <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+                    <FileUploadInput
+                        name="documents"
+                        acceptTypes={acceptTypes}
+                        maxFiles={5}
+                        maxSizeInMB={5}
+                        label="upload docs"
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </FormProvider>
         </div>
     )
 }
